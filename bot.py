0bbc312b59f794e3c72560688979bc227bc1abda
SadsHelp <96987569+Yones2007@users.noreply.github.com>
2025-06-21 14:09:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 async def start(update, context):
     await update.message.reply_text("صلي على محمد ﷺ")
 
-app = ApplicationBuilder().token(os.environ["8067374289:AAHmgMpF2Mhe7Scc1c8-g5ywYYlNdNEWF_Q"]).build()
+app = ApplicationBuilder().token(os.environ["BOT_TOKEN"]).build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
